b0cebc7baa2e6c3b286bd62e45bbab5f5901c4bc
Duc <nguyenthanhduc242004@gmail.com>
2026-01-13 12:58:17 +0700

    fix: add 'TO_PAY' status to formatStatus.

--- frontend/src/utils/formatStatus.ts
+++ frontend/src/utils/formatStatus.ts
@@ -1,6 +1,7 @@
 export const formatStatus = (status: string): string => {
   const map: Record<string, string> = {
     TO_CONFIRM: 'To Confirm',
+    TO_PAY: 'To Pay',
     TO_PICKUP: 'To Pickup',
     SHIPPING: 'Shipping',
     COMPLETED: 'Completed',
