8c240a6463384c3d08cd7d043f71effa10541de6
phamdatzx <megumikatoubuhbuh@gmail.com>
2026-04-26 09:39:46 +0700

    refactor: delete wrong rag

--- services/ai-service/app/services/ai-rag/recreate_collection.py
+++
@@ -1,9 +0,0 @@
-from qdrant_client import QdrantClient
-from qdrant_client.models import VectorParams, Distance
-
-client = QdrantClient(host="localhost", port=6333)
-
-client.recreate_collection(
-    collection_name="docs",
-    vectors_config=VectorParams(size=384, distance=Distance.COSINE)
-)
